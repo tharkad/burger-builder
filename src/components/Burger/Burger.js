bb79a6734b8481b523fcaef19aaca1db455760db
Mike Houser <tharkad@casdra.com>
2019-07-05 18:49:57 -0700

    Ingreidents in state and dynamically rendering them

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,6 +3,13 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngrediant';
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            });
+        });
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,19 @@ import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingrediets: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render () {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingrediets}/>
                 <div>Build Controls</div>
             </Aux>
         )
